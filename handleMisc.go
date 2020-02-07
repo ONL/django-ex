a21dd726f5bf4622970b5b0babb18524373d3c25
ONL <chinook-media@gmx.de>
2020-02-07 13:58:36 +0100

    Update handleMisc.go

--- handleMisc.go
+++ handleMisc.go
@@ -5,9 +5,30 @@ import (
 )
 
 func quellenHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "quellen", "base")
+	return renderTemplate(w, "quellen", "base")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about", "base")
+	return renderTemplate(w, "about", "base")
+}
+
+// homeHandler handles all requests, as other handlers redirect here with added
+// parameters in env.
+func homeHandler(env *Env, w http.ResponseWriter, r *http.Request) error {
+    
+    w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+
+    username, password, authOK := r.BasicAuth()
+    
+    if false == authOK {
+        return errors.New(http.StatusText(http.StatusUnauthorized))
+    }
+
+    if password != *env.args["Password"] { {
+        return errors.New(http.StatusText(http.StatusUnauthorized))
+    }
+
+    fmt.Fprintf(w, "%+v", username)
+    w.Header().Set("X-Forwarded-User", username)
+    return renderTemplate(w, "home", "base")
 }
