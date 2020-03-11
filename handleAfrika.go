b7596cb16b984a0cce40c24bb41890667ea8b835
ONL <chinook-media@gmx.de>
2020-03-11 12:34:58 +0100

    Update handleAfrika.go

--- handleAfrika.go
+++ handleAfrika.go
@@ -31,18 +31,18 @@ func afrikaKlimaHandler(env *Env, w http.ResponseWriter, r *http.Request) error 
         content.score = 5
         renderTemplate(w, "afrika_klima", "base", content)
   } else {
-        if '1' == r.PostForm.Get('afgnr') {
-            if 'posted' == r.PostForm.Get('status') {
-                if 'subtrop' == r.PostForm.Get('cat1') {
+        if "1" == r.PostForm.Get('afgnr') {
+            if "posted" == r.PostForm.Get('status') {
+                if "subtrop" == r.PostForm.Get('cat1') {
                     content.score = content.score + 1
                 }
-                if 'passat' == r.PostForm.Get('cat2') {
+                if "passat" == r.PostForm.Get('cat2') {
                     content.score = content.score + 1
                 }
-                if 'wechsel' == r.PostForm.Get('cat3') {
+                if "wechsel" == r.PostForm.Get('cat3') {
                     content.score = content.score + 1
                 }
-                if 'aequatorial' == r.PostForm.Get('cat4') {
+                if "aequatorial" == r.PostForm.Get('cat4') {
                     content.score = content.score + 1
                 }
                 content.cat1 = r.PostForm.Get('cat1')
@@ -77,24 +77,24 @@ func afrikaVegetationHandler(env *Env, w http.ResponseWriter, r *http.Request) e
 	content.score = 7
         return renderTemplate(w, "afrika_vegetation", "base", content)
   } else {
-        if '1' == r.PostForm.Get('afgnr') {
-            if 'posted' == r.PostForm.Get('status') {
-                if 'hartlaub' == r.PostForm.Get('cat1') {
+        if "1" == r.PostForm.Get('afgnr') {
+            if "posted" == r.PostForm.Get('status') {
+                if "hartlaub" == r.PostForm.Get('cat1') {
                     content.score = content.score + 1
                 }
-                if 'wuste' == r.PostForm.Get('cat2') {
+                if "wuste" == r.PostForm.Get('cat2') {
                     content.score = content.score + 1
                 }
-                if 'dornenstrauch' == r.PostForm.Get('cat3') {
+                if "dornenstrauch" == r.PostForm.Get('cat3') {
                     content.score = content.score + 1
                 }
-                if 'trockensavanne' == r.PostForm.Get('cat4') {
+                if "trockensavanne" == r.PostForm.Get('cat4') {
                     content.score = content.score + 1
                 }
-                if 'feuchtsavanne' == r.PostForm.Get('cat5') {
+                if "feuchtsavanne" == r.PostForm.Get('cat5') {
                     content.score = content.score + 1
                 }
-                if 'tropR' == r.PostForm.Get('cat6') {
+                if "tropR" == r.PostForm.Get('cat6') {
                     content.score = content.score + 1
                 }
                 content.cat1 = r.PostForm.Get('cat1')
