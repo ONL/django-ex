c2407fe4f1cb66d759e6160928677a17fe7151cf
ONL <chinook-media@gmx.de>
2020-03-11 15:02:07 +0100

    Update handleMisc.go

--- handleMisc.go
+++ handleMisc.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"net/http"
+	"fmt"
+	"errors"
 )
 
 func quellenHandler(env *Env, w http.ResponseWriter, r *http.Request) error {
-	return renderTemplate(w, "quellen", "base")
+	return renderTemplate(w, "quellen", "base", nil)
 }
 
 func aboutHandler(env *Env, w http.ResponseWriter, r *http.Request) error {
-	return renderTemplate(w, "about", "base")
+	return renderTemplate(w, "about", "base", nil)
 }
 
 // homeHandler handles all requests, as other handlers redirect here with added
@@ -30,5 +32,5 @@ func homeHandler(env *Env, w http.ResponseWriter, r *http.Request) error {
 
     fmt.Fprintf(w, "%+v", username)
     w.Header().Set("X-Forwarded-User", username)
-    return renderTemplate(w, "index", "base")
+    return renderTemplate(w, "index", "base", nil)
 }
